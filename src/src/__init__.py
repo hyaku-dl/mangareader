4805bec5e842aa8d82678072515607dcdd017df8
whi~nyaan! <whinyaan@gmail.com>
2023-10-28 22:45:28 +0800

    For info, check `docs/latest commit.md` or this commit's comments Release notes: https://mrt.hyaku.download/changelog#0-0-0-0-0-2 or `docs/latest release notes.md`

--- src/src/__init__.py
+++ src/src/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "0.0.0.0-alpha.1"
-hver = "0.0.1-alpha.1"
-vls = [0, 0, 0, 0, 0, 1]
+__version__ = "0.0.0.0-alpha.2"
+hver = "0.0.1-alpha.2"
+vls = [0, 0, 0, 0, 0, 2]
 appimage = False
 cholder = """Copyright for portions of project <a target=_blank
 href="https://github.com/hyaku-dl/mangareader">mangareader</a> is held by [whi_ne, Github account <a target=_blank
